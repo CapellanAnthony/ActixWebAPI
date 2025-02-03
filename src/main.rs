45aee9ea607edd4082bbb2543458cdcc89b51d99
Stiven Guillermo Bueno Reynoso <anthonycapellan03@gmail.com>
2025-02-02 23:56:07 -0400

    Second

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod model;
 mod repository;
 
 use api::task::{
-    get_task
+    get_task,submit_task,start_task,fail_task,complete_task,pause_task
 };
 
 use actix_web::{HttpServer, App,web::Data, middleware::Logger};
@@ -29,9 +29,14 @@ async fn main() -> std::io::Result<()> {
 
         let logger = Logger::default();
         App::new()
-        .app_data(ddb_data)
         .wrap(logger)
-        .service(get_task);
+        .app_data(ddb_data)
+        .service(get_task)
+        .service(submit_task)
+        .service(fail_task)
+        .service(pause_task)
+        .service(start_task)
+        .service(complete_task)
     })
     .bind(("127.0.0.1",8080))?
     .run()
--- src/model/task.rs
+++ src/model/task.rs
@@ -1,8 +1,9 @@
 use serde::Serialize;
 use uuid::Uuid;
-use strum_macros::{EnumString,Display};
+use strum_macros::{EnumString, Display};
 
-pub enum TasktState{
+#[derive(Serialize, EnumString, Display, Debug, Eq, PartialEq)]
+pub enum TaskState {
     NotStarted,
     InProgress,
     Completed,
@@ -10,34 +11,33 @@ pub enum TasktState{
     Failed
 }
 
-#[derive(Deserialize)]
-pub struct Task{
+#[derive(Serialize, Debug)]
+pub struct Task {
     pub user_uuid: String,
     pub task_uuid: String,
     pub task_type: String,
     pub state: TaskState,
     pub source_file: String,
-    pub result_file: Option<String>,
-
+    pub result_file: Option<String>
 }
 
-impl Task{
-    pub fn new(user_uuid:String,task_type:String, source_file:String) -> Task{
+impl Task {
+    pub fn new(user_uuid: String, task_type: String, source_file: String) -> Task {
         Task {
             user_uuid,
-            task_uuid:Uuid::new_v4().to_string(),
+            task_uuid: Uuid::new_v4().to_string(),
             task_type,
-            state: TasktState::NotStarted,
+            state: TaskState::NotStarted,
             source_file,
-            result_file:None
+            result_file: None
         }
     }
 
-    pub fn get_global_id(&self) -> String{
-        format!("{}_{}",self.user_uuid,self.task_id);
+    pub fn get_global_id(&self) -> String {
+        return format!("{}_{}", self.user_uuid, self.task_uuid);
     }
 
-    pub fn can_transition_to(&self,state:&TasktState) -> bool{
-        self.state != *state;
+    pub fn can_transition_to(&self, state: &TaskState) -> bool {
+        self.state != *state
     }
 }
